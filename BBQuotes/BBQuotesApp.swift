1d4b8871a51e0b819f9edac94f4542210439db9e
milorusso <milorusso@deloitte.it>
2024-12-11 14:34:59 +0100

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Lorusso, Michele on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
